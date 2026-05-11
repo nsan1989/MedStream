49ea817a5ff836c8dd894809ae72d83402483eb7
nsan1989 <nsanajaoba@yahoo.com>
2026-05-11 16:14:08 +0530

    med_stream view v.0.1

--- med_stream/accounts/urls.py
+++ med_stream/accounts/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import authView
 
 urlpatterns = [
-    path("auth/", authView, name="auth"),
+    path("auth/register/", authView, name="register"),
+    path("auth/login/", authView, name="login"),
 ]
